c429d405f81d9c5c5f24bede6b2566ceea1e3f71
Bryan Amds <bryan.amadeus1003@gmail.com>
2024-06-06 22:53:54 +0700

    UI + MASSIVE weather improvement

--- script.js
+++ script.js
@@ -20,13 +20,17 @@ const weather_api = `https://api.openweathermap.org/data/2.5/weather?lat=-6.2000
 fetch(weather_api)
     .then(response => response.json())
     .then(data => {
-        const weather = data.weather[0].main;
-        const weather_desc = data.weather[0].description;
+        const weather_name = data.weather[0].description.toUpperCase();
         const temp = data.main.temp;
+        const icon = data.weather[0].icon;
+        const humidity = data.main.humidity;
+        const wind_speed = data.wind.speed;
 
-        document.getElementById('weatherName').innerText = `${weather}`;
-        document.getElementById('weatherDesc').innerText = `Description: ${weather_desc}`;
+        document.getElementById('icon-container').innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="" width="200px">`
+        document.getElementById('weather_name').innerText = `${weather_name}`;
         document.getElementById('temperature').innerText = `${temp}°C`;
+        document.getElementById('humidity').innerText = `${humidity}%`;
+        document.getElementById('wind_speed').innerText = `${wind_speed} km/s`;
     })
 
 
